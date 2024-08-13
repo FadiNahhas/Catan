ba6f3abdcb8dc903dec5d9cd366c321db8e9cd00
Fadi Nahhas <fadi.nahhas1@gmail.com>
2024-08-13 15:47:56 +0300

    Main Menu update
    - Game browser scroll view setup
    - Improved menu animation script and variables

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,34 +6,50 @@ namespace UI
 {
     public class MainMenu : MonoBehaviour
     {
+        private const float TargetXPos = 0f;
+        
         [TabGroup("References"), SerializeField, Required] private RectTransform mainMenu;
         [TabGroup("References"), SerializeField, Required] private RectTransform settingsMenu;
         [TabGroup("References"), SerializeField, Required] private RectTransform gameBrowser;
         
         [TabGroup("Settings"), SerializeField] private float animationDuration = 0.5f;
         [TabGroup("Settings"), SerializeField] private Ease animationEase;
+        
+        #region Initial Positions
+
+        private float _settingsMenuInitialXPos;
+        private float _gameBrowserInitialXPos;
+        
+        #endregion
+
+        private void OnEnable()
+        { 
+            _settingsMenuInitialXPos = settingsMenu.anchoredPosition.x;
+            _gameBrowserInitialXPos = gameBrowser.anchoredPosition.x;
+        }
+
         public void OpenSettings()
         {
-            mainMenu.DOAnchorPosX(1200f, animationDuration).SetEase(animationEase);
-            settingsMenu.DOAnchorPosX(0f, animationDuration).SetEase(animationEase);
+            mainMenu.DOAnchorPosX(-_settingsMenuInitialXPos, animationDuration).SetEase(animationEase);
+            settingsMenu.DOAnchorPosX(TargetXPos, animationDuration).SetEase(animationEase);
         }
 
         public void CloseSettings()
         {
-            mainMenu.DOAnchorPosX(0f, animationDuration).SetEase(animationEase);
-            settingsMenu.DOAnchorPosX(-1200f, animationDuration).SetEase(animationEase);
+            mainMenu.DOAnchorPosX(TargetXPos, animationDuration).SetEase(animationEase);
+            settingsMenu.DOAnchorPosX(_settingsMenuInitialXPos, animationDuration).SetEase(animationEase);
         }
 
         public void OpenGameBrowser()
         {
-            mainMenu.DOAnchorPosX(-1200f, animationDuration).SetEase(animationEase);
-            gameBrowser.DOAnchorPosX(0f, animationDuration).SetEase(animationEase);   
+            mainMenu.DOAnchorPosX(_settingsMenuInitialXPos, animationDuration).SetEase(animationEase);
+            gameBrowser.DOAnchorPosX(TargetXPos, animationDuration).SetEase(animationEase);   
         }
         
         public void CloseGameBrowser()
         {
-            mainMenu.DOAnchorPosX(0f, animationDuration).SetEase(animationEase);
-            gameBrowser.DOAnchorPosX(1700f, animationDuration).SetEase(animationEase);
+            mainMenu.DOAnchorPosX(TargetXPos, animationDuration).SetEase(animationEase);
+            gameBrowser.DOAnchorPosX(_gameBrowserInitialXPos, animationDuration).SetEase(animationEase);
         }
         
         public void QuitGame() => Application.Quit();
